422ad1fc7259deeb0ad95f0d2039c08fda85c1d2
ParaschivMihai21 <148899985+ParaschivMihai21@users.noreply.github.com>
2023-10-26 11:18:40 +0300

    Add files via upload

---
+++ 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// add method to sort by name
+	public List<Employee> findAllByOrderByLastNameAsc();
+}
